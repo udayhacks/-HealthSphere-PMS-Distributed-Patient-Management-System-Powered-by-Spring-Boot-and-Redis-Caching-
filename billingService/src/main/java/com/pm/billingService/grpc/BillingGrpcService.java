24725f06e0b214e56a376f81375a19f958a2830e
alone_coder <blackdevil190402@gmail.com>
2025-12-22 15:10:07 +0530

    local Grpc billing service module completed

---
+++ billingService/src/main/java/com/pm/billingService/grpc/BillingGrpcService.java
@@ -0,0 +1,38 @@
+package com.pm.billingService.grpc;
+
+import billing.BillRequest;
+import billing.BillResponse;
+import billing.BillingServiceGrpc.BillingServiceImplBase;
+
+import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+
+
+@GrpcService
+public class BillingGrpcService  extends BillingServiceImplBase {
+
+
+    private  static  final Logger log =  LoggerFactory.getLogger(BillingGrpcService.class);
+
+
+
+    @Override
+    public  void createBillingAccount(BillRequest billRequest, StreamObserver<BillResponse> responseObserver){
+        log.info("createBillingAccount request recived "+billRequest.toString());
+        ///  business logic : save to database
+
+        BillResponse response = BillResponse.newBuilder()
+                .setAccountId("1234")
+                .setStatus("ACTIVE")
+                .build();
+
+        responseObserver.onNext(response); // send response to client that prepared we call mutilple as we want ;
+        responseObserver.onCompleted();
+    }
+
+
+}
